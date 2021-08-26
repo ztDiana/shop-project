7abd2dfc8c65dac672fbae284677c52dcb3026f6
diana <dpidhaina@gmail.com>
2021-08-26 18:54:41 +0300

    made hidden modal

--- app.js
+++ app.js
@@ -12,10 +12,51 @@ addToCartBtns.forEach(item => ( item.addEventListener("click",function(){
 
 // homework 
 let like=document.querySelectorAll(".like")
-console.log(like)
+
 
 like.forEach(item => (item.addEventListener("click", function(){
         item.classList.toggle("clicked-like")
 })
 )
 )
+//modal block
+
+let more_details_btn=document.querySelectorAll(".btn-more-details");
+let modal=document.querySelector(".modal");
+let closeModal_btn=document.querySelector(".btn-close");
+let closedModal=modal
+more_details_btn.forEach(item=> 
+        item.addEventListener("click", function (){     
+                document.body.appendChild(modal)
+                modal.classList.add("show")
+                modal.classList.remove("hide")
+                
+         })
+        );
+closeModal_btn.addEventListener("click", close);
+
+
+
+function open(){
+                modal.classList.add("show")
+                modal.classList.remove("hide")
+                
+         };
+function close(){
+        modal.classList.remove("show")
+        modal.classList.add("hide")
+         };
+
+modal.addEventListener("click", function(e){
+        if(e.target===modal){
+                close();      
+        }
+})
+window.addEventListener("scroll",function(){
+        if(window.pageYOffset>=300){
+                open()
+        }
+})
+closeModal_btn.addEventListener("click", function(){
+        modal.remove()
+});
